b0ee0f240dcfe498ee30963da0ff0cd8bfffd092
Sherlock Holmes <1056937524@qq.com>
2019-04-17 17:14:12 +0800

    完善

--- vue是如何实现变化侦测的/vue.js
+++ vue是如何实现变化侦测的/vue.js
@@ -1,69 +1,93 @@
-class Dep {
-    constructor() {
-        this.subscribers = []
+let data= {price:5,quality:10}
+let total =0
+var setTotal = function (){
+    total = data.price * data.quality
+}
+
+class Dep { 
+    constructor(){
+        this.subscribers= [] 
     }
-    depend() {
-        if (target && !this.subscribers.includes(target)) {
-            this.subscribers.push(target)
+    addDep(target){
+        this.subscribers.push(target)
+    }
+    depend(){
+        if (Dep.target && !this.subscribers.includes(Dep.target)){
+            this.addDep(Dep.target)
         }
     }
-    notify() {
-        this.subscribers.forEach(sub => sub())
+    notify(){
+        const arr = this.subscribers.slice()
+        arr.forEach(t=> t())
     }
 }
-let data = { price: 5, quantity: 2 }
-let total = 0
-let target = null //匿名函数
+Dep.target = null
+const dep = new Dep()
+let value = data.price
 
-Object.keys(data).forEach(key => {
-    let internalValue = data[key]
-    const dep = new Dep()
-    Object.defineProperty(data, key, {
-        get() { //访问属性的时候，记住属性所在匿名函数
-            console.log(`${key}被访问了`)
-            dep.depend()
-            return internalValue
-        },
-        set(newVal) { //更新属性的时候，运行保存的匿名函数
-            console.log(`我被改变了,改变后${newVal}`)
-            internalValue = newVal
-            dep.notify()
-        }
-    })
+Object.defineProperty(data, 'price',{
+    configurable:true,
+    enumerable:true,
+    get(){
+        dep.depend()
+        return value
+    },
+    set(newVal){
+        value = newVal
+        dep.notify()
+    }
 })
 
-// record = () => { storage.push(target) } //记录函数的方法
-// replay = () => { storage.forEach(run => run()) }//运行函数的方法
-function watcher(myFunc) { 
-    target = myFunc
-    target()
-    target = null
-}
-watcher(() => { 
-    total = data.price * data.quantity
-})
-watcher(() => { 
-    salePrice = data.price * 0.9
-})
-console.log(`total is ${total}`)
-console.log(`total is ${salePrice}`)
+Dep.target = setTotal
+setTotal()
+Dep.target  = null
 
-data.price = 20
-console.log(`total is ${total}`)
-console.log(`total is ${salePrice}`)
+console.log(total)
+data.price = 10
 
-data.quantity = 3
-console.log(`total is ${total}`)
-console.log(`total is ${salePrice}`)
+console.log(total)
+// function defineReactive(data,key ,val){
+//     const dep = new Dep()
+//     Object.defineProperty(data,key,{
+//         configurable: true,
+//         enumerable: true,
+//         get() {
+//             dep.depend()
+//             return par
+//         },
+//         set(newVal) {
+//             par = newVal
+//             dep.notify()
+//         }
+//     })
+// }
+// Object.keys(data).forEach(key=>{
+//     const dep = new Dep()
+//     let par = data[key]
+//     Object.defineProperty(data,key,{
+//         configurable:true,
+//         enumerable:true,
+//         get(){
+//             dep.depend()
+//             return par
+//         },
+//         set(newVal){
+//             par = newVal
+//             dep.notify()
+//         }
+//     })
+// })
+// Dep.target = null
 
+// function watch(fun){
+//     Dep.target = fun
+//     fun()
+//     Dep.target = null
+// }
 
-
-
-
-data.price
-data.price = 20
-
-
-
-
-
+// watch(()=>{
+//     total =  data.price * data.quality
+// })
+// console.log(total)
+// data.price = 10
+// console.log(total)
--- vue是如何实现变化侦测的/vue2.0.js
+++ vue是如何实现变化侦测的/vue2.0.js
@@ -1,47 +1,93 @@
-class Dep { 
-    constructor(){ 
-        this.subscriber = []//订阅者数组
+
+class Dep{
+    constructor(){
+       this.subscriber=[] //订阅者数组
+       
     }
-    depend() { 
-        if (target && !this.subscriber.includes(target)) { 
-            this.subscriber.push(target)
+    depend(){
+        if (Dep.target && !this.subscriber.includes(Dep.target)){
+            this.subscriber.push(Dep.target)
         }
     }
-    notify() { 
-        let subs = this.subscriber.slice()
-        subs.forEach(sub => sub())
+    notify(){
+        const subs = this.subscriber.slice()
+        subs.forEach(sub => sub.update())
     }
 }
-let target = null
-
-let watcher = (fun) => { 
-    target = fun
-    target()
-    target = null
+Dep.target = null;
+class Observer { //观察者
+    constructor(value) {
+        this.value = value
+        if(!Array.isArray(value)){
+            this.walk(value)
+        }
+    }
+    walk(obj) {
+        Object.keys(obj).forEach(key=>{
+            defineReactive(obj,key,obj[key])
+        })
+    }
 }
-let data = { price: 5, quality: 10 }
-let total = 0
-Object.keys(data).forEach(key => {
-    let val = data[key]
+function defineReactive(data,key,val){
+    if(typeof val === 'object'){
+        new Observer(val)
+    }
     const dep = new Dep()
-    Object.defineProperty(data, key, {
-        get() {
+    Object.defineProperty(data,key,{
+        configurable:true,
+        enumerable:true,
+        get(){
             dep.depend()
             return val
         },
-        set(newVal) {
+        set(newVal){
+            if(val===newVal){
+                return
+            }
             val = newVal
             dep.notify()
         }
     })
-})
+}
+
+class Watcher{ //中介
+    constructor(vm,expOrFn,cb){//vm只是vue的实例
+        this.vm = vm
+        if (typeof expOrFn === 'function') {
+            this.getter = expOrFn;
+        }
+        this.cb = cb
+        this.value = this.get()
+    }
+    get(){
+        Dep.target = this
+        let value = this.getter.call(this.vm,this.vm)
+        Dep.target = undefined
+        return value
+    }
+    update(){
+        const oldVal = this.value
+        this.value = this.get()
+        this.cb.call(this.vm, this.value, oldVal)
+    }
+}
 
 
-watcher(() => { 
-    total = data.price * data.quality
-})
+let data = { price: 5, quality: 10 }
+let total = 0
+const updateComponent = () => { 
+    total = data.price * data.quality 
+    return total
+}
+
+new Observer(data)
+new Watcher(window, updateComponent,  (newVal,oldVal)=>{
+    console.log(newVal)
+    console.log(oldVal)
+});
 
 console.log(total)
-data.price = 10
-
+data.price = 12
+console.log(total)
+data.quality = 13
 console.log(total)
